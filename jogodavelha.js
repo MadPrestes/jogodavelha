a6f9c9e05507747ebadd92ac9daba01fa9f2d067
Otavio Prestes <otavio.prestes@gmail.com>
2018-04-28 23:28:59 -0300

    modified:   index.html 	modified:   jogodavelha.js

--- jogodavelha.js
+++ jogodavelha.js
@@ -6,8 +6,6 @@ jvapp.controller('jvControl',['$scope', function($scope){
     var c = 0;
     var aide;
     var jv = $scope;
-    var temp;
-  
        
     var bt1 = 1;
     var bt2 = 2;
@@ -19,41 +17,41 @@ jvapp.controller('jvControl',['$scope', function($scope){
     var bt8 = 8;
     var bt9 = 9;
 
+  
+
+    
      $scope.clicktest = function(clickEvent){
-         console.log(bt1, bt2, bt3, bt4, bt5 ,bt6 , bt7, bt8 ,bt9);
-             //linhas  
-             if( bt1 == bt2 && bt3 && !null){
-                alert(bt1 + ' ganhou');
-                
-                console.log('linha1')
-            };
-            if (bt4 == bt5 && bt6 && !null){
-                alert(bt4 + ' ganhou');
-                console.log('linha2')
-            };
-            if (bt7 == bt8 && bt9 && !null){
-                alert(bt7 + ' ganhou'); 
-                console.log('linha3')
-            };
+//             console.log(bt1, bt2, bt3, bt4, bt5 ,bt6 , bt7, bt8 ,bt9);
+                                if(jv.b1b != null ){bt1 = jv.b1b;};
+                                if(jv.b2b != null ){bt2 = jv.b2b;};
+                                if(jv.b3b != null ){bt3 = jv.b3b;};
+                                if(jv.b4b != null ){bt4 = jv.b4b;};
+                                if(jv.b5b != null ){bt5 = jv.b5b;};
+                                if(jv.b6b != null ){bt6 = jv.b6b;};
+                                if(jv.b7b != null ){bt7 = jv.b7b;};
+                                if(jv.b8b != null ){bt8 = jv.b8b;};
+                                if(jv.b9b != null ){bt9 = jv.b9b;};
+
+
+
+         //linhas  
+             if(bt1 === bt2 && bt1 === bt3 && bt1 !== null){alert(bt1 + ' ganhou');};
+            if (bt4 == bt5 && bt4 == bt6 && bt4 !== null){alert(bt4 + ' ganhou');};
+            if (bt7 == bt8 && bt7 == bt9 && bt7 !== null){alert(bt7 + ' ganhou');};
             
             
         // contraBarra
-            if (bt1 == bt5 && bt9){ alert(bt5 + '  ganhou');console.log('contrabarra')};
+            if (bt1 == bt5 && bt1 == bt9 && bt5 !== null){alert(bt5 + '  ganhou');};
             // barra
-            if (bt7 == bt5 && bt3){alert(bt3 + '  ganhou'); console.log('barra')};
+            if (bt7 == bt5 && bt7 == bt3 && bt7 !== null){alert(bt3 + '  ganhou');};
     
         // colunas
-            if(bt1 == bt4 && bt7){alert(bt1 + '  ganhou'); console.log('coluna1')};
-            if(bt2 == bt5 && bt8){alert(bt8 + '   ganhou'); console.log('coluna2')};
-            if(bt3 == bt6 && bt9 && !null){alert(bt9 + '   ganhou'); console.log('coluna3')};
-          
-  
-          
+            if(bt1 == bt4 && bt1 == bt7 && bt1 !== null){alert(bt1 + '  ganhou'); };
+            if(bt2 == bt5 && bt2 == bt8 && bt2 !== null){alert(bt8 + '   ganhou');};
+            if(bt3 == bt6 && bt3 == bt9 && bt3 !== null){alert(bt9 + '   ganhou');};
     };
 
     
-
-
     $scope.showID = function(event){
         aide = event.target.id;
         if(c == 0){j = 0};
@@ -67,7 +65,6 @@ jvapp.controller('jvControl',['$scope', function($scope){
         if(c == 8){j = 0};
         if(c == 9){c = 0};  
         c++;
-
                         if(aide == "b1"){$scope.b1b = player[j];};
                         if(aide == "b2"){$scope.b2b = player[j];};
                         if(aide == "b3"){$scope.b3b = player[j];};
@@ -77,41 +74,6 @@ jvapp.controller('jvControl',['$scope', function($scope){
                         if(aide == "b7"){$scope.b7b = player[j];};
                         if(aide == "b8"){$scope.b8b = player[j];};
                         if(aide == "b9"){$scope.b9b = player[j];};
-
-                        bt1 = jv.b1b;
-                        bt2 = jv.b2b;
-                        bt3 = jv.b3b;
-                        bt4 = jv.b4b;
-                        bt5 = jv.b5b;
-                        bt6 = jv.b6b;
-                        bt7 = jv.b7b;
-                        bt8 = jv.b8b;
-                        bt9 = jv.b9b;
-                              
     }
-    
 }]);
 
-/*
-linha1 i1, i2, i3
-linha2 i4, i5, i6
-linha3 i7, i8, i9
-
-coluna1 i1, i4, i7
-coluna2 i2, i5, i8
-coluna3 i3, i6, i9
-
-barra i7, i5, i3
-
-contraBarra i1, i5, i9
-*/
-
- /*if(c = 0){j = 0};
-    if(c = 1){j = 1};
-    if(c = 2){j = 0};
-    if(c = 3){j = 1};
-    if(c = 4){j = 0};
-    if(c = 5){j = 1};
-    if(c = 6){j = 0};
-    if(c = 7){j = 1};
-    if(c = 8){j = 0};*/
